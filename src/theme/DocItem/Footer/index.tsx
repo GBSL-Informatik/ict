49765d50d5279442fb4d4b6f396f79b7ceb033ff
bh0fer <lebalz@outlook.com>
2026-05-23 11:36:40 +0200

    simple rating system

--- src/theme/DocItem/Footer/index.tsx
+++ src/theme/DocItem/Footer/index.tsx
@@ -5,25 +5,29 @@ import type { WrapperProps } from '@docusaurus/types';
 import styles from './style.module.scss';
 import { useDoc } from '@docusaurus/plugin-content-docs/client';
 import clsx from 'clsx';
-import Icon from '@mdi/react';
-import { mdiOpenInNew } from '@mdi/js';
+import { mdiStar, mdiStarOutline } from '@mdi/js';
 import client from '@tdev-components/directusClient';
-import { aggregate, readItems } from '@directus/sdk';
+import { aggregate, readItems, createItem } from '@directus/sdk';
+import Button from '@tdev-components/shared/Button';
+import { useLocation } from '@docusaurus/router';
 
 type Props = WrapperProps<typeof FooterType>;
 const CLIENT_ID = 'f149d3f7-1d34-42b7-ae77-2a14ae8a1905';
 
 export default function FooterWrapper(props: Props): ReactNode {
     const { frontMatter } = useDoc();
+    const { pathname } = useLocation();
+    const [rating, setRating] = React.useState<{ rating: number; id: number } | null>(null);
+    const [pageRatings, setPageRatings] = React.useState<{ rating: number; count: number }[]>([]);
     const { page_id } = frontMatter as {
         page_id: string;
     };
     React.useEffect(() => {
         const load = async () => {
-            const ratings = await client.request(
+            const ratings = client.request(
                 aggregate('ict_page_ratings', {
                     aggregate: {
-                        count: '*'
+                        count: 'id'
                     },
                     query: {
                         filter: {
@@ -35,15 +39,64 @@ export default function FooterWrapper(props: Props): ReactNode {
                     groupBy: ['rating']
                 })
             );
-            return ratings;
+            const myRating = client.request(
+                readItems('ict_page_ratings', {
+                    filter: {
+                        page_id: {
+                            _eq: page_id
+                        },
+                        client_id: {
+                            _eq: CLIENT_ID
+                        }
+                    },
+                    sort: ['-created_at']
+                })
+            );
+            const [ratingsRes, myRatingRes] = await Promise.all([ratings, myRating]);
+            return {
+                ratings: ratingsRes,
+                myRating: myRatingRes[0]
+            };
         };
         load().then((ratings) => {
-            console.log(ratings);
+            setPageRatings(
+                ratings.ratings.map((r) => ({ rating: r.rating, count: r.count ? Number(r.count) : 0 }))
+            );
+            setRating(ratings.myRating ? { rating: ratings.myRating.rating, id: ratings.myRating.id } : null);
         });
-    }, []);
+    }, [page_id]);
     return (
         <div className={styles.footer}>
-            <div className={styles.sources}></div>
+            <hr />
+            <div className={styles.ratingContainer}>
+                Wie hilfreich war diese Seite?
+                <div className={clsx(styles.rating)}>
+                    {[1, 2, 3, 4, 5].map((i) => {
+                        const isActive = i <= (rating?.rating ?? 0);
+                        return (
+                            <Button
+                                icon={isActive ? mdiStar : mdiStarOutline}
+                                color={isActive ? 'gold' : 'var(--ifm-color-secondary-darkest)'}
+                                key={i}
+                                onClick={() => {
+                                    client
+                                        .request(
+                                            createItem('ict_page_ratings', {
+                                                page_id,
+                                                rating: i,
+                                                pathname: pathname,
+                                                client_id: CLIENT_ID
+                                            })
+                                        )
+                                        .then((res) => {
+                                            setRating({ rating: i, id: res.id });
+                                        });
+                                }}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
             <Footer {...props} />
         </div>
     );
--- website/components/directusClient.ts
+++ website/components/directusClient.ts
@@ -1,5 +1,13 @@
 import { createDirectus, authentication, rest } from '@directus/sdk';
 
-const client = createDirectus('https://directus.gbsl.website').with(rest());
+interface Rating {
+    id: number;
+    rating: number;
+    page_id: string;
+    pathname: string;
+    client_id: string;
+    created_at: string;
+}
+const client = createDirectus<{ ict_page_ratings: Rating[] }>('https://directus.gbsl.website').with(rest());
 
 export default client;
