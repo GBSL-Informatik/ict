ca6c4fbc861c3c19ccdde12480741297760f9073
bh0fer <lebalz@outlook.com>
2026-05-24 11:15:04 +0200

    add spinner

--- packages/ict/directus/components/PageRating/index.tsx
+++ packages/ict/directus/components/PageRating/index.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import styles from './styles.module.scss';
 import { useStore } from '@tdev-hooks/useStore';
 import { observer } from 'mobx-react-lite';
-import { mdiStar, mdiStarOutline } from '@mdi/js';
+import { mdiStar, mdiStarOutline, mdiLoading } from '@mdi/js';
 import Button from '@tdev-components/shared/Button';
 import { useLocation } from '@docusaurus/router';
 
@@ -11,6 +11,19 @@ interface Props {
     pageId: string;
     label?: string;
 }
+
+const getIconPath = (starNr: number, rating?: number) => {
+    if (!rating) {
+        return mdiStarOutline;
+    }
+    if (rating < 0 && starNr === Math.abs(rating)) {
+        return mdiLoading;
+    }
+    if (starNr <= Math.abs(rating)) {
+        return mdiStar;
+    }
+    return mdiStarOutline;
+};
 
 const PageRating = observer((props: Props) => {
     const { pageId } = props;
@@ -33,7 +46,7 @@ const PageRating = observer((props: Props) => {
                     const isActive = i <= Math.abs(rating);
                     return (
                         <Button
-                            icon={isActive ? mdiStar : mdiStarOutline}
+                            icon={getIconPath(i, rating)}
                             color={isActive ? 'gold' : 'var(--ifm-color-secondary-darkest)'}
                             key={i}
                             spin={rating === -i}
